bf2076395659d9fd4bce39dcefa2ea009d17f170
BOMBELLA <nmasionyena@gmail.com>
2026-04-20 18:46:53 +0100

    power task

---
+++ TouchGrass.java/Power.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class Power{
+
+       public static void main(String[] args){
+
+        Scanner inputPower = new Scanner(System.in);
+
+        System.out.print("Enter a power:");
+
+         int power = inputPower.nextInt();
+
+         int digit = 1;     
+
+           for(digit = 1;digit <= power;digit++){
+                     
+                 System.out.println(digit);
+
+                      }
+              
+
+
+    }
+
+
+
+
+
+
+
+}
