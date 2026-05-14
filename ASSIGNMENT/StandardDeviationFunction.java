dc5c2ae914b1eac8e205a6fc9fbe0300e8f44ef5
BOMBELLA <nmasionyena@gmail.com>
2026-05-14 04:28:31 +0100

    deviation

--- ASSIGNMENT/StandardDeviationFunction.java
+++ ASSIGNMENT/StandardDeviationFunction.java
@@ -1,39 +1,67 @@
 public class StandardDeviationFunction{
 
-       public static void main(String [] args){
-
-        int [] numbers = {2,4,1,3,2,6};
-
-        int result =  sumOfMeanInTheGivenNumbers(numbers);
-
-              System.out.println(result);      
+            public static void main(String [] args){
+//
+//        int [] numbers = {2,4,1,3,2,6};
+//
+//        int result =  sumOfMeanInTheGivenNumbers(numbers);
+//
+//              System.out.println(result);      
        }
 
 
-       public static int SumOfMeanInTheGivenNumbers(int [] numbers){
-              int  sumOfMean = 0;
+       public static int meanInTheGivenNumbers(int [] numbers){
+              int  sumOfNumbers = 0;
+              int mean = 0;
+              int count = 0;
 
-              for(int count = 0; count < numbers.length; count++){
+              for(count = 0; count < numbers.length; count++){
 
-                     sumOfMean += numbers[count];
-              }
-
-        return sumOfMean;      
+                     sumOfNumbers += numbers[count];
+                     mean = sumOfNumbers / count;
+          }    
+        return mean;      
         }
 
 
+       public static int sumOfGivenNumbers(int [] numbers){
+              int  sumOfNumbers = 0;
+              int count = 0;
+
+            for(count = 0; count < numbers.length; count++){
+
+                   sumOfNumbers += numbers[count];
+              
+
+       }
+      return sumOfNumbers;                    
+
+       }
+
+     
+
+       public static int [] substractMeanFromEachValue(int [] numbers){
+
+              int mean = 3;
+//               int  substraction = 0;
+//
+//              int substractMeanFromEachValue = 0;
+//
+               int count = 0;
+
+              int [] result = new int[numbers.length];
+
+              for(count = 0; count < numbers.length; count++){
+
+               result[count] = numbers[count] - mean;
+
+//         substractMeanFromEachValue = mean - numbers[count];
 
 
-
-
-
-
-
-
-
-
-
-
+              
+       }
+       return result;
+}
 
 
 
--- ASSIGNMENT/StandardDeviationTest.java
+++ ASSIGNMENT/StandardDeviationTest.java
@@ -4,11 +4,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StandardDeviationTest{
 
-    @Test
-    public void testForTheSumOfMeanInTheGivenNumbers(){
+//    @Test
+//    public void testForTheMeanInTheGivenNumbers(){
+//
+//        int [] numbers = {2,4,1,3,2,6};
+//        int expectedAverage = 3;
+//        int actualAverage = StandardDeviationFunction.meanInTheGivenNumbers(numbers);
+//        assertEquals(actualAverage, expectedAverage);
+//       } 
+//
+//
+//     @Test
+//     public void testForTheSumOfGivenNumbers(){     
+//
+//       int [] numbers = {2,4,1,3,2,6};
+//       int expectedLength = 18;
+//       int actualLength = StandardDeviationFunction.sumOfGivenNumbers(numbers);
+//       assertEquals(actualLength, expectedLength);
+//      }
 
-        int [] numbers = {2,4,1,3,2,6};
-        int expectedAverage = 3;
-        int actualAverage = StandardDeviation.geTheMeanInTheGivenNumbers(numbers);
-        assertEquals(actualAverage, expectedAverage);
-    } 
+        @Test
+        public void testTosubstractMeanFromEachValue(){
+
+
+       
+       int [] numbers = {2,4,1,3,2,6};
+
+       int [] expectedLength = {-1,1,-2,0,-1, 3};
+//       int expectedLength = {1, -1, 2, 0,1, -3};
+       int [] actualLength = StandardDeviationFunction.substractMeanFromEachValue(numbers);
+       assertArrayEquals(actualLength, expectedLength); 
+
+       }    
+
+}
+
+
+
+
+
+
+
+//}
